bbcdf89ff5f9d7bed1be2ac2848ea56fdae0431d
Aleksandr Shakhmatov <Coluchy@yandex.ru>
2025-03-06 01:16:25 +0600

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -100,8 +100,9 @@ class HomePage:
     @allure.step('Проверка элемента презентации')
     def check_presentation(self):
         browser.open('')
-        if browser.element(css_or_xpath_or_by="/html/body/div[3]/div[2]/div/div[2]/span"):
-            browser.element('[class ="b-block-cookie-notice__text"]').click()
+        browser.open('')
+        if browser.element('[class ="b-block-cookie-notice__close"]'):
+            browser.element('[class ="b-block-cookie-notice__close"]').click()
         browser.element('[class="b-block-announcement"]').perform(
                 command.js.scroll_into_view).should(be.visible)
         browser.element(css_or_xpath_or_by="/html/body/div[3]/main/div[2]/div[12]/span").should(
