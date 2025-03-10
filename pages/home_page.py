1403946930a5c7e3c9398000bb69842c04c5381a
Aleksandr Shakhmatov <Coluchy@yandex.ru>
2025-03-11 04:26:17 +0600

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -154,9 +154,10 @@ class HomePage:
         browser.element(css_or_xpath_or_by="/html/body/div[3]/main/div[2]/h1").should(
             have.text('Цифровой и ИТ-консалтинг'))
         browser.driver.back()
+        browser.element('[class = "h3"]').perform(command.js.scroll_into_view).should(be.visible)
         browser.element("a[href*='/solutions/services/informatsionnaya-bezopasnost/']").should(
             have.text('Информационная безопасность'))
-        browser.element"a[href*='/solutions/services/informatsionnaya-bezopasnost/']").click()
+        browser.element(css_or_xpath_or_by="/html/body/div[3]/main/div[2]/div[2]/div/map/area[9]").click()
         browser.element(css_or_xpath_or_by="/html/body/div[3]/main/div[2]/h1").should(
             have.text('Информационная безопасность'))
         browser.driver.back()
