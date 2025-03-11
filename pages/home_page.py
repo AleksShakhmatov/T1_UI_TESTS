4d7098ea129c86572be1bcea90936246923f9b5d
AleksSh <Coluchy@yandex.ru>
2025-03-12 02:35:03 +0600

    new

--- pages/home_page.py
+++ pages/home_page.py
@@ -128,7 +128,7 @@ class HomePage:
         browser.driver.back()
         browser.element("a[href*='/solutions/services/bolshie-dannye-klientskiy-opyt-i-ai-ml/']").should(
             have.text('Большие данные, клиентский опыт и AI/ML'))
-        browser.driver.get()
+        browser.driver.set_page_load_timeout(15)
         browser.element(css_or_xpath_or_by="/html/body/div[3]/main/div[2]/div[2]/div/map/area[11]").click()
         browser.element(css_or_xpath_or_by="/html/body/div[3]/main/div[2]/h1").should(
             have.text('Большие данные, клиентский опыт и AI/ML'))
